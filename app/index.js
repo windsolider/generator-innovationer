2231b390708aa77c74b8327f7b9983d76176c9aa
windsolider <windsolider@163.com>
2017-05-22 12:02:47 +0800

    增加gulpfile,测试是否能生成文件

---
+++ app/index.js
@@ -0,0 +1,30 @@
+let generators = require('yeoman-generator');
+let fs = require('fs');
+let path = require('path');
+
+module.exports = generators.Base.extend(
+	constructor: function() {
+
+	},
+	prompting: function() {
+		let questions = [
+			{
+				type: 'input',
+				name: 'projectName',
+				message: '输入项目名称',
+				default: this.app.name
+			}
+		];
+		return this.prompt(questions).then(function(answers) {
+			for (let item in answers) {
+				answers.hasOwnProperty(item) && (this[item] = answers[item]);
+			}
+		}.bind(this));
+	},
+	writing: function() {
+		this.copy('gulpfile.js', 'gulpfile.js');
+	},
+	end: function() {
+
+	}
+); 
